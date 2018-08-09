99c3883be59315cbb5d30356141feec31ccd8380
Felipe Lira <felipedrl@gmail.com>
2018-08-09 16:19:29 +0200

    Added renderer scene view overrider

---
+++ Assets/_Completed/SceneViewOverrider.cs
@@ -0,0 +1,36 @@
+﻿namespace UnityEngine.Experimental.Rendering.LightweightPipeline
+{
+    public static class SceneViewOverrider
+    {
+        public static void AddRendererSetup(IRendererSetup setup)
+        {
+            Camera[] cameras = Resources.FindObjectsOfTypeAll<Camera>();
+            for (var i = 0; i < cameras.Length; i++)
+            {
+                Camera cam = cameras[i];
+
+                if (cam.cameraType != CameraType.Game)
+                {
+                    if (cam.gameObject.GetComponent(setup.GetType()) == null)
+                    {
+                        cam.gameObject.AddComponent(setup.GetType());
+                    }
+                }
+            }
+        }
+
+        public static void RemoveRendererSetup(IRendererSetup setup)
+        {
+            Camera[] cameras = Resources.FindObjectsOfTypeAll<Camera>();
+            for (var i = 0; i < cameras.Length; i++)
+            {
+                Camera cam = cameras[i];
+
+                if (cam.cameraType != CameraType.Game)
+                {
+                    GameObject.DestroyImmediate(cam.gameObject.GetComponent(setup.GetType()));
+                }
+            }
+        }
+    }
+}
