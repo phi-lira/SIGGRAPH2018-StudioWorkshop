85028a4609561f16097ce275c01bade20a80288e
Felipe Lira <felipe@unity3d.com>
2018-08-10 17:04:01 +0200

    Changed CustomPass to render a nyan cat overlay

---
+++ Assets/Shaders/CustomBlit.shader
@@ -0,0 +1,45 @@
+﻿Shader "Hidden/SIGGRAPH Studio/CustomBlit"
+{
+	Properties
+	{
+		_BlitTex("BlitTexture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderPipeline" = "LightweightPipeline" }
+		
+		Pass
+	    {
+	        Blend SrcAlpha OneMinusSrcAlpha
+			HLSLPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "LWRP/ShaderLibrary/Core.hlsl"
+			
+			TEXTURE2D(_BlitTex);
+			SAMPLER(sampler_BlitTex);
+			
+			struct VertexOutput
+			{
+			    float4 positionCS : SV_POSITION;
+			    float3 uv0 : TEXCOORD0;
+			};
+			
+			VertexOutput vert (float4 positionOS : POSITION, float uv0 : TEXCOORD0)
+			{
+			    VertexOutput OUT;
+				OUT.positionCS = TransformObjectToHClip(positionOS.xyz);
+				OUT.uv0 = uv0;
+				return OUT;
+			}
+			
+			half4 frag (VertexOutput IN) : SV_Target
+			{
+			    half3 color = SAMPLE_TEXTURE2D(_BlitTex, sampler_BlitTex, IN.uv0).rgb;
+			    return half4(color, 0.5);
+			}
+			ENDHLSL
+		}
+	}
+}
--- Assets/_Completed/CustomPassGetter.cs
+++ Assets/_Completed/CustomPassGetter.cs
@@ -1,42 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Experimental.Rendering;
-using UnityEngine.Experimental.Rendering.LightweightPipeline;
-using UnityEngine.Rendering;
+﻿using UnityEngine.Rendering;
 
-public class CustomPassGetter : MonoBehaviour, IAfterOpaquePass
+namespace UnityEngine.Experimental.Rendering.LightweightPipeline
 {
-	private CustomPass pass = new CustomPass();
-	
-	public ScriptableRenderPass GetPassToEnqueue(RenderTextureDescriptor baseDescriptor, RenderTargetHandle colorAttachmentHandle,
-		RenderTargetHandle depthAttachmentHandle)
+	public class CustomPassGetter : MonoBehaviour, IAfterOpaquePass
 	{
-		pass.Setup(colorAttachmentHandle, depthAttachmentHandle);
-		return pass;
-	}
-}
+		public Texture2D m_OverlayTexture;
+		private CustomPass pass = new CustomPass();
 
-
-public class CustomPass : ScriptableRenderPass
-{
-	public override void Execute(ScriptableRenderer renderer, ref ScriptableRenderContext context, ref CullResults cullResults,
-		ref RenderingData renderingData)
-	{
-		CommandBuffer cmd = CommandBufferPool.Get("Render");
-		cmd.SetRenderTarget(colorhandle.Identifier(), depthHandle.Identifier());
-		cmd.ClearRenderTarget(true, true, Color.blue);
-		context.ExecuteCommandBuffer(cmd);
-		CommandBufferPool.Release(cmd);
+		public ScriptableRenderPass GetPassToEnqueue(RenderTextureDescriptor baseDescriptor,
+			RenderTargetHandle colorAttachmentHandle,
+			RenderTargetHandle depthAttachmentHandle)
+		{
+			pass.Setup(colorAttachmentHandle, m_OverlayTexture);
+			return pass;
+		}
 	}
 
-	public void Setup(RenderTargetHandle colorAttachmentHandle, RenderTargetHandle depthAttachmentHandle)
+
+	public class CustomPass : ScriptableRenderPass
 	{
-		this.colorhandle = colorAttachmentHandle;
-		this.depthHandle = depthAttachmentHandle;
+		public override void Execute(ScriptableRenderer renderer, ref ScriptableRenderContext context,
+			ref CullResults cullResults,
+			ref RenderingData renderingData)
+		{
+			if (m_Material == null)
+				m_Material = CoreUtils.CreateEngineMaterial(Shader.Find("Hidden/SIGGRAPH Studio/CustomBlit"));
+			m_Material.SetTexture("_BlitTex", m_OverlayTexture);
+
+			if (m_OverlayTexture != null)
+			{
+				MaterialPropertyBlock props = new MaterialPropertyBlock();
+				props.SetTexture("_BlitTex", m_OverlayTexture);
+			}
+			
+			CommandBuffer cmd = CommandBufferPool.Get("Render Overlay");
+			cmd.Blit(m_OverlayTexture, colorAttachment.Identifier(), m_Material);
+			context.ExecuteCommandBuffer(cmd);
+			CommandBufferPool.Release(cmd);
+		}
+
+		public void Setup(RenderTargetHandle colorAttachmentHandle, Texture2D overlayTexture)
+		{
+			colorAttachment = colorAttachmentHandle;
+			m_OverlayTexture = overlayTexture;
+		}
+
+		private Material m_Material = null;
+		private RenderTargetHandle colorAttachment;
+		private Texture2D m_OverlayTexture;
+
 	}
-
-	RenderTargetHandle colorhandle { get; set; }
-
-	RenderTargetHandle depthHandle { get; set; }
 }
